9f568fd384eb6528a8a805fa241c5d0e74b92a55
Abdulmajeed Jaafer <abdulmajeedjaafer76@gmail.com>
2021-04-03 11:21:30 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 function writeCards(names, eventName){
   const result = [];
   for (let i = 0; i < names.length; i++) {
-    result.push('Thank you, ' + names[i] +', for the wonderful '+eventName+' gift!')
+    result.push('Thank you, ' + names[i] +', for the wonderful '+eventName+' gift!');
   }
+  return result;
 }
