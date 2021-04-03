ccb7a45a6f3aac544b0e8a83709f6aa4e7a2faf9
Abdulmajeed Jaafer <abdulmajeedjaafer76@gmail.com>
2021-04-03 11:21:17 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Code your solutions in this file
+function writeCards(names, eventName){
+  const result = [];
+  for (let i = 0; i < names.length; i++) {
+    result.push('Thank you, ' + names[i] +', for the wonderful '+eventName+' gift!')
+  }
+}
